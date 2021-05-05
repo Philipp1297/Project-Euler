f2f892e27a81415a50d55216d2acec592f0608eb
Philipp1297 <pschinacher@protonmail.com>
2021-05-05 12:55:07 +0200

    add test file and solved problem 4

--- src/problem1to10.py
+++ src/problem1to10.py
@@ -13,14 +13,24 @@ for i in fibonacci_numbers:
 
 x= sum([i for i in liste if i%2==0 and i>0])
 
-# Problem 3
+# Problem 3 	Largest prime factor
 x = 600851475143
 k = 1
 while k < x:
     k += 1
     while x % k == 0: 
         x = x // k
-print(k)
+#print(k) # k is solution
+
+# Prblem 4 	Largest palindrome product
+liste=[]
+for i in range(100, 999):
+    for j in range(100, 999):
+        x= i*j
+        if str(x) == str(x)[::-1]:
+            liste.append(x)
+
+#print(max(liste))
 
 
 
