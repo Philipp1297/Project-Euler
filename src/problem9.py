c08b169e1e95276b9b588d0964f126b804a5e535
Philipp1297 <pschinacher@protonmail.com>
2021-05-05 18:28:47 +0200

    solved problem 9

---
+++ src/problem9.py
@@ -0,0 +1,8 @@
+for i in range(0,1000):
+    for j in range(0,1000):
+        for k in range(0,1000):
+            if i<j<k:
+                if ((i*i + j*j) == k*k):
+                    if i+j+k==1000:
+                        print(i," ",j," ",k)
+                        print(i*j*k)
