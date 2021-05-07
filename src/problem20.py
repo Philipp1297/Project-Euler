18d30d17dbd669d0133d1599352a1be49c1f8e55
Philipp1297 <pschinacher@protonmail.com>
2021-05-07 08:20:49 +0200

    solved problem 20

---
+++ src/problem20.py
@@ -0,0 +1,15 @@
+# Problem 20
+
+def fact(n):
+    fact=1
+    for i in range(1,n+1):
+        fact = fact * i
+    return fact
+
+x= fact(100)
+x=str(x)
+erg=0
+for i in x:
+    erg+=int(i)
+
+print(erg)
